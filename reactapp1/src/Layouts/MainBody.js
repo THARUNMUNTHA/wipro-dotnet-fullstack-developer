2d0f8c350d46c0966371079484c7789f0e2c5abe
THARUNMUNTHA <116761277+THARUNMUNTHA@users.noreply.github.com>
2025-03-19 12:55:10 +0530

    Add files via upload

---
+++ reactapp1/src/Layouts/MainBody.js
@@ -0,0 +1,17 @@
+
+const MainBody=(props)=>
+{
+  
+  return (
+          <div>
+          <p>In this course we learn react by building {props.variable1} for {props.count} times </p>
+          <ul>
+          <li>Call Ben</li>
+          <li>Go to walmart</li>
+          </ul>
+          </div>
+      )
+}
+
+
+export {MainBody}
